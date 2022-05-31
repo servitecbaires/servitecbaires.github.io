78247ac0df969f287d5c1e81f7cace02caad03be
servitecbaires <69863183+servitecbaires@users.noreply.github.com>
2022-05-31 17:31:30 -0300

    Create principal.js

---
+++ tp-integrador/principal.js
@@ -0,0 +1,14 @@
+const valorTicket=200
+function calculoVenta(){
+    let cant=document.getElementById("cant").value
+    let categoria= document.getElementById("categoria").value
+    let descuento
+    if (categoria==1)
+        {descuento=80}
+    else if (categoria==2)
+        {descuento=50}
+    else 
+        {descuento=15}
+    let total=valorTicket*cant
+    total -= total*descuento/100
+    document.getElementById("total").innerHTML=total
